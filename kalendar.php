fffdf60d44685f9c19f413ff74c2e983b2e2c80b
helaveesa <helaveesa.nolder@gmail.com>
2013-09-30 16:52:04 +0400

    modified file golosphp add div class and css add kalendar

--- kalendar.php
+++ kalendar.php
@@ -3,29 +3,53 @@
  <head>
    <title>Календарь</title>
   <meta charset="utf-8">
-    <style type="text/css">
-   TABLE {
-    border-collapse: collapse; /* Убираем двойные линии между ячейками */
-    width: 300px; /* Ширина таблицы */
-   }
-   TH { 
-    background: #fc0; /* Цвет фона ячейки */
-    text-align: left; /* Выравнивание по левому краю */
-   }
-   TD {
-    background: #fff; /* Цвет фона ячеек */
-    text-align: center; /* Выравнивание по центру */
-   }
-   TH, TD {
-    border: 1px solid black; /* Параметры рамки */
-    padding: 4px; /* Поля вокруг текста */
-   }
-   </style>
  </head> 
+ 
  <body>
   <table>
-   <tr><th>Понедельник</th><td>Ячейка 1</td><td>Ячейка 2</td></tr>
-   <tr><th>Вторник</th><td>Ячейка 3</td><td>Ячейка 4</td></tr>
+   <tr>
+        <th>Понедельник</th>
+            <td>Ячейка 1</td>
+            </tr>
+            <td>Ячейка 2</td>
+            </tr>
+            <td>Ячейка 3</td>
+            </tr>
+            <td>Ячейка 4</td>
+            </tr>
+            <td>Ячейка 5</td>
+            </tr>
+            <td>Ячейка 6</td>
+    </tr>
+    <tr>
+        <th>Вторник</th>
+            <td>Ячейка 1</td>
+            </tr>
+            <td>Ячейка 2</td>
+            </tr>
+            <td>Ячейка 3</td>
+            </tr>
+            <td>Ячейка 4</td>
+            </tr>
+            <td>Ячейка 5</td>
+            </tr>
+            <td>Ячейка 6</td>
+    </tr>
+    <tr>
+        <th>Среда</th>
+            <td>Ячейка 1</td>
+            </tr>
+            <td>Ячейка 2</td>
+            </tr>
+            <td>Ячейка 3</td>
+            </tr>
+            <td>Ячейка 4</td>
+            </tr>
+            <td>Ячейка 5</td>
+            </tr>
+            <td>Ячейка 6</td>
+    </tr>
+   
   </table>
  </body>
 </html>
