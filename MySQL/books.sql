12c10d814b0ff546e72cdf0d3c264fd4f0cd5f91
helaveesa <helaveesa.nolder@gmail.com>
2013-10-08 12:21:46 +0400

    ADD NEW directory MySQL add 2 files books.sql uchet.sql
    Работа с БД

---
+++ MySQL/books.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Окт 07 2013 г., 21:20
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `lybrary`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `books`
+--
+
+CREATE TABLE IF NOT EXISTS `books` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `author` varchar(255) NOT NULL,
+  `nazv` varchar(255) NOT NULL,
+  `god_izd` year(4) NOT NULL,
+  `izdat` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=cp1251 AUTO_INCREMENT=5 ;
+
+--
+-- Дамп данных таблицы `books`
+--
+
+INSERT INTO `books` (`id`, `author`, `nazv`, `god_izd`, `izdat`) VALUES
+(1, 'Nabokov', 'Live', 1999, 'Art'),
+(2, 'Nabokov', 'Live', 1999, 'Art'),
+(3, 'Nabokov', 'Live', 1999, 'Art'),
+(4, 'Nabokov', 'Live', 1999, 'Art');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
