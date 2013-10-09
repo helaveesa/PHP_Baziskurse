318ff3d98f9b7fa5e3c5727d7b40198a7a2bbf0b
helaveesa <helaveesa.nolder@gmail.com>
2013-10-09 16:20:02 +0400

    add files into dir ITOG again

--- templates/templates_select.php
+++ templates/templates_select.php
@@ -6,17 +6,22 @@
   <meta charset="utf-8">
   <title>Тег SELECT</title>
  </head>
- <body>  
  
+ <body>  
+ <h2>Выберите героя</h2>
   <form action="select1.php" method="post">
-   <p><select size="3" multiple name="hero[]">
+  
+   <select size="3" multiple name="hero[]">
+   
     <option disabled>Выберите героя</option>
     <option value="Чебурашка">Чебурашка</option>
     <option selected value="Крокодил Гена">Крокодил Гена</option>
     <option value="Шапокляк">Шапокляк</option>
     <option value="Крыса Лариса">Крыса Лариса</option>
-   </select></p>
-   <p><input type="submit" value="Отправить"></p>
+   </select>
+   
+   <!--отправка фомы -->
+	<input type="submit" value="Отправить">
   </form>
 
  </body>
--- templates/templates_textarea_pole_for_text.php
+++ templates/templates_textarea_pole_for_text.php
@@ -37,9 +37,10 @@ wrap
  <body>
 
   <form action="textarea1.php" method="post">
-    <p><b>Введите ваш отзыв:</b></p>
-    <p><textarea rows="10" cols="45" name="text"></textarea></p>
-    <p><input type="submit" value="Отправить"></p>
+    <br><b>Введите ваш отзыв:</b>
+    </br>
+    <textarea rows="10" cols="45" name="text"></textarea>
+    <input type="submit" value="Отправить">
   </form>
 
  </body>
