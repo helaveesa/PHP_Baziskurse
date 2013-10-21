ec0fb60190236edab81d8932ec4fe41a6a310e3a
helaveesa <helaveesa.nolder@gmail.com>
2013-10-21 14:10:46 +0400

    modified file pars_serebr.php

--- pars_serebr.php
+++ pars_serebr.php
@@ -1,13 +1,15 @@
 Я этот делал на клиенте, так проще. По поводу авторизации. Самое просто - выдрать заголовки из браузера. Или вот тут есть ответ для curl'a (рекомендую им и пользоваться)
-нормально, спасибо)
-Кирилл • 20 мин. назад
-Кирилл Серебрянный
+<?php
 if(preg_match('/href=".*">/i', $temp, $result)){
         $out['link'][$i] = 'http://industrialconflicts.ru/msg/'.substr($result[0], 6, strlen($result[0])-8);
     }
+?>
 1. SHD, кажется, имеет встроенный поиск по атрибутам
 2. trailing slash лишний, там все ссылки от корня указаны, кажется
+<?php
 foreach($html->find('a') as $element) 
-       echo $element->href . '<br>';   
+       echo $element->href . '<br>'; 
+?>       
 прям на главной странице))
-   http://simplehtmldom.sourceforge.net/   
+
+http://simplehtmldom.sourceforge.net/   
