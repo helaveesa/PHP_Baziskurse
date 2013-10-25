f17be451ac9be7a593e3d9896f7476eef028fb44
helaveesa <helaveesa.nolder@gmail.com>
2013-10-25 14:35:16 +0400

    lessons from video curse YT
    img files and php files registraciya, array, switch, include and require

---
+++ add_files/footer.php
@@ -0,0 +1,15 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Footer сайта</title>
+</head>
+
+<body>
+<?php
+
+echo '<hr><p style = \'text-align: center;\'>Контакты | Copyright &copy; 2013 www.motoistka.ru</p>'; // &copy; - знак авторского права на языке html
+
+?>
+</body>
+</html>
---
+++ add_files/header.php
@@ -0,0 +1,13 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Шапка сайта</title>
+</head>
+
+<body>
+<?php
+echo '<h1 style = "text-align: center;">ШАПКА САЙТА</h1><hr>'; // <hr> - горизонтальная линия
+?>
+</body>
+</html>
