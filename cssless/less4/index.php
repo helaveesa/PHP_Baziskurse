5c418efc56f067705ce644da848905ab2e5b738c
helaveesa <helaveesa.nolder@gmail.com>
2013-12-11 14:45:26 +0400

    add new lessons form css
    css sorax video

---
+++ cssless/less4/index.php
@@ -0,0 +1,33 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>css lesson 4</title>
+    
+    <link rel="stylesheet" href="main.css" type="text/css" />
+</head>
+
+<body>
+    <div class="conteiner">
+    	<div class="conteiner-inner">Содержимое внутреннего контейнера</div>    
+    </div>
+    <div class="conteiner2">
+    	<div class="conteiner-inner2">Содержимое внутреннего контейнера</div>    
+    </div>
+    <div class="conteiner">
+    	<div class="conteiner-inner">Содержимое внутреннего контейнера</div>
+    </div>
+    <div class="conteiner">
+    	<div class="conteiner-inner">Содержимое внутреннего контейнера</div>
+    </div>
+    <div class="conteiner">
+    	<div class="conteiner-inner">Содержимое внутреннего контейнера</div>
+    </div>
+    <a href="#">Ссылка</a>
+    <br>
+    <div class="conteiner3">
+    	При работе с cookies необходимо учитывать важный момент, заключающийся в том, что cookie надо обязательно устанавливать перед отправкой в броузер каких-либо заголовков, поскольку сами cookies устанавливаются в виде заголовков. Поэтому если установить cookies после какого-либо текста, отправляемого в броузер, то возникнет ошибочная ситуация.   
+    </div>
+
+</body>
+</html>
